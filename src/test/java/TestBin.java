2737ede4bec93be3b8bc7403af3902b65e11c472
val <valfragier@gmail.com>
2019-10-25 21:00:40 -0400

    Updated Test

--- src/test/java/TestBin.java
+++ src/test/java/TestBin.java
@@ -23,19 +23,23 @@ public class TestBin {
 
         Bins secondBin = new Bins(13);
 
-        for(int i = 1; i <= 100; i++){
+        for(int i = 1; i <= 100000; i++){
             secondBin.increment(die1.roll());
         }
 
-        for(int i = 0; i < 13; i++){
+        for(int i = 2; i <= 12; i++){
             // System.out.println(i + " : " + secondBin.getNum(i));
 
-            double percentage = secondBin.getNum(i) / 100.0;
+            double percentage = secondBin.getNum(i) / 100000.0;
 
             String output = String.format("%1$2d: %2$10d: ", i , secondBin.getNum(i)) +
                     String.format("%.2f :", percentage);
 
-            System.out.println(output);
+            System.out.print(output);
+            for(int j = 0; j < (percentage*100); j++){
+                System.out.print("*");
+            }
+            System.out.println();
 
         }
 
